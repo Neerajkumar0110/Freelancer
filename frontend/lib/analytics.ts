35cdb2d2a8e96ca2cad2fade0efa3064ca715866
Altaf Raja <altafrajavines@gmail.com>
2026-02-19 00:23:23 +0530

    fixed

--- frontend/lib/analytics.ts
+++
@@ -1,7 +0,0 @@
-export function track(event: string, props?: Record<string, any>) {
-  console.log("[Analytics]", event, props);
-
-  // Later:
-  // window.gtag?.("event", event, props);
-  // posthog.capture(event, props);
-}
